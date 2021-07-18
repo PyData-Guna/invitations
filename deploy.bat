31c5c05874a2beb2d64a84059554dc75b630eade
Param Siddharth <contact@paramsid.com>
2021-07-18 15:43:51 +0530

    Update deployment scripts

--- deploy.bat
+++ deploy.bat
@@ -3,25 +3,36 @@
 setlocal enabledelayedexpansion
 
 ::Install all production modules
+echo Installing all production modules...
 npm ci --only=production
 
 ::Create .nojekyll
+echo Ensuring .nojekyll...
 if not exist .nojekyll (
 	copy nul .nojekyll >nul || goto :error
 )
 
 ::Delete .gitignore
+echo Deleting .gitignore...
 if exist .gitignore (
 	del /f /q /s .gitignore >nul || goto :error
 )
 
 ::Commit and push
+echo Staging files...
 git add .                                        || goto :error
+echo Committing...
 git commit --allow-empty-message --no-edit       || goto :error
+echo Pushing...
 git push origin main:gh-pages                    || goto :error
 
 ::Reset one commit back
+echo Resetting to previous commit...
 git reset --hard HEAD~                           || goto :error
+
+::Resetting dependencies
+echo Resetting dependencies...
+npm ci                                           || goto :error
 
 endlocal
 
