57b1cca2af78bebfa7b6da60bf01cb60f194bbb1
Param Siddharth <contact@paramsid.com>
2021-07-18 19:52:02 +0530

    Add Google Form link

---
+++ 2021/wshp/cybersec1/res/script.js
@@ -0,0 +1,10 @@
+console.log(`Hello, World!`);
+console.log(`\
+If you have spotted me here, you've earned yourself a chance
+to be even more excited about the workshop.
+
+This workshop is for people who are curious to learn about
+cybersecurity, how to break into systems, how to design
+systems that can't be broken into, et cetera.
+
+Go ahead and register!`);
